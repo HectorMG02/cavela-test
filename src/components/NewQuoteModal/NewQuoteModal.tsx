714c85a4042634c274f85485ede2fe7cdaef06d9
Hector <hector.matias@ecam.es>
2024-03-29 16:54:06 +0100

    open modal

--- src/components/NewQuoteModal/NewQuoteModal.tsx
+++ src/components/NewQuoteModal/NewQuoteModal.tsx
@@ -5,30 +5,10 @@ const NewQuoteModal = ( { onClose }: { onClose: () => void}) => {
    
     return (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="new-quote-modal">
-          <div className="relative top-20 mx-auto p-5 border w-11/12 md:w-3/4 lg:w-1/2 shadow-lg rounded-md bg-white">
+          <div className="relative top-10 mx-auto p-5 border w-[90%] h-[80%] shadow-lg rounded-md bg-white">
             <div className="mt-3 text-center">
-              <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-                {/* Icon or Image */}
-              </div>
-              <h3 className="text-lg leading-6 font-medium text-gray-900">New Quote</h3>
-              <div className="mt-2 px-7 py-3">
-                {/* Content goes here: this is where you would add the form fields as per your layout */}
-    
-                <div className="mt-4">
-                  <button
-                    onClick={onClose}
-                    className="bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-700"
-                  >
-                    Close
-                  </button>
-                  <button
-                    onClick={() => { /* Handle Save Here */ }}
-                    className="bg-blue-500 text-white py-2 px-4 rounded float-right hover:bg-blue-700"
-                  >
-                    Save Quote
-                  </button>
-                </div>
-              </div>
+            <h3 className="text-2xl leading-6 font-medium text-gray-900">Create A New Quote</h3>
+            
             </div>
           </div>
         </div>
