c844a61be9da5b2cead9e4c0405f8475dc695018
Hector <hector.matias@ecam.es>
2024-03-30 13:11:40 +0100

    added more data to the table

--- src/components/Tables/SupplierTable/logic.ts
+++ src/components/Tables/SupplierTable/logic.ts
@@ -1,7 +1,36 @@
 const useLogic = () => {
     const supplierData = [
-        { variant: 'Black Ash', quantity: 250, cost: '$1', leadTime: '35 days', moq: 250, sampleCost: '$50' },
-        { variant: 'Brown Ash', quantity: 250, cost: '$1.50', leadTime: '40 days', moq: 250, sampleCost: '$60' },
+       {
+            name: "Supplier 1",
+            quotes: [
+                { variant: 'Black Ash', quantity: 250, cost: '$1', leadTime: '35 days', moq: 250, sampleCost: '$50' },
+                { variant: 'Brown Ash', quantity: 250, cost: '$1.50', leadTime: '40 days', moq: 250, sampleCost: '$60' },
+            ]
+       },
+       {
+            name: "Supplier 2",
+            quotes: [
+                { variant: 'Black Ash', quantity: 250, cost: '$1.20', leadTime: '35 days', moq: 250, sampleCost: '$50' },
+                { variant: 'Brown Ash', quantity: 250, cost: '$1.70', leadTime: '40 days', moq: 250, sampleCost: '$60' },
+                { variant: 'White Ash', quantity: 250, cost: '$1.70', leadTime: '40 days', moq: 250, sampleCost: '$60' },
+                { variant: 'Grey Ash', quantity: 250, cost: '$1.70', leadTime: '40 days', moq: 250, sampleCost: '$60' },
+            ]
+       },
+       {
+        name: "Supplier 3",
+        quotes: [
+            { variant: 'Black Ash', quantity: 250, cost: '$1.20', leadTime: '35 days', moq: 250, sampleCost: '$50' },
+            { variant: 'Brown Ash', quantity: 250, cost: '$1.70', leadTime: '40 days', moq: 250, sampleCost: '$60' },
+            { variant: 'White Ash', quantity: 250, cost: '$1.70', leadTime: '40 days', moq: 250, sampleCost: '$60' },
+        ]
+        },
+        {
+            name: "Supplier 4",
+            quotes: [
+                { variant: 'Black Ash', quantity: 250, cost: '$1.20', leadTime: '35 days', moq: 250, sampleCost: '$50' },
+                { variant: 'Brown Ash', quantity: 250, cost: '$1.70', leadTime: '40 days', moq: 250, sampleCost: '$60' },
+            ]
+       }
       ];
       
     return {
