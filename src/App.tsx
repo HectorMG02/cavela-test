4889f5d361a0e512e55be77274efcc2bfb9ebcb0
Hector <hector.matias@ecam.es>
2024-04-01 19:20:46 +0200

    tests created

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     return (
         <>
             <Provider store={store}>
-            <QuotesScreen />
+                <QuotesScreen />
             </Provider>
         </>
     );
