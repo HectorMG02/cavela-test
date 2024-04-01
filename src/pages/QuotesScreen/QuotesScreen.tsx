bca221cb623484fc5d03bd428f614701fb90d34b
Hector <hector.matias@ecam.es>
2024-04-01 19:26:44 +0200

    app working

--- src/pages/QuotesScreen/QuotesScreen.tsx
+++ src/pages/QuotesScreen/QuotesScreen.tsx
@@ -67,7 +67,7 @@ const QuotesScreen = () => {
                 <h1 className="text-4xl mb-6">Quotes Selection</h1>
                 <div className="h-1 bg-gray-300 mb-6 w-[95%]" />
             </div>
-            <div className="grid grid-cols-3 gap-10 w-[95%]">
+            <div className="grid md:grid-cols-3 gap-10 w-[95%]">
                 {loading ? renderSkeletons() : renderCards()}
             </div>
         </>
